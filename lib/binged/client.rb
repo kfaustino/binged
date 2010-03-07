5960bd32af230694886a7e0ab34a57d2c116d131
Kevin Faustino <kevin.faustino@gmail.com>
2010-03-07 09:29:03 -0500

    Add web search access from client

--- lib/binged/client.rb
+++ lib/binged/client.rb
@@ -1,13 +1,17 @@
 module Binged
-  
+
   class Client
-    
+
     attr_accessor :api_key
-    
+
     def initialize(options = {})
       @api_key = options[:api_key] || Binged.api_key
     end
-        
+
+    def web(query='')
+      Search::Web.new(self,query)
+    end
+
   end
-  
+
 end
--- spec/binged_spec.rb
+++ spec/binged_spec.rb
@@ -2,13 +2,18 @@ require 'spec_helper'
 
 describe "Binged" do
 
-  it "should configure the api_key for easy access" do    
+  it "should configure the api_key for easy access" do
     Binged.configure do |config|
       config.api_key = 'api_key'
     end
-    
-    @client = Binged::Client.new
-    @client.api_key.should == 'api_key'
+
+    client = Binged::Client.new
+    client.api_key.should == 'api_key'
   end
-  
+
+  it "should provide a simple interface to web search" do
+    client = Binged::Client.new
+    client.web.should be_instance_of(Binged::Search::Web)
+  end
+
 end
