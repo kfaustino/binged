8fb5d76362f40b04a870144dac24f4972bb450cc
Kevin Faustino <kevin.faustino@gmail.com>
2010-03-15 10:01:50 -0400

    Specify return value of Image filters

--- lib/binged/search/image.rb
+++ lib/binged/search/image.rb
@@ -17,18 +17,23 @@ module Binged
       end
 
       # Restrict images to those small in size
+      # 
+      # @return [self]
       def small
         filter << 'Size:Small'
         self
       end
 
       # Restrict images to those medium in size
+      # 
+      # @return [self]
       def medium
         filter << 'Size:Medium'
         self
       end
 
       # Restrict images to those large in size
+      # @return [self]
       def large
         filter << 'Size:Large'
         self
@@ -37,6 +42,7 @@ module Binged
       # Restrict images to the specified height in pixels
       #
       # @param [Fixnum] pixels height in pixels
+      # @return [self]
       def height(pixels)
         filter << "Size:Height:#{pixels}"
         self
@@ -45,60 +51,79 @@ module Binged
       # Restrict images to the specified width in pixels
       #
       # @param [Fixnum] pixels width in pixels
+      # @return [self]
       def width(pixels)
         filter << "Size:Width:#{pixels}"
         self
       end
 
       # Restrict images to those that have a square aspect ratio
+      # 
+      # @return [self]
       def square
         filter << 'Aspect:Square'
         self
       end
 
       # Restrict images to those that have a wide aspect ratio
+      # 
+      # @return [self]
       def wide
         filter << 'Aspect:Wide'
         self
       end
 
       # Restrict images to those that have a tall aspect ratio
+      # 
+      # @return [self]
       def tall
         filter << 'Aspect:Tall'
         self
       end
       
       # Restrict images to those that are in color
+      # 
+      # @return [self]
       def color
         filter << 'Color:Color'
         self      
       end
       
       # Restrict images to those which contain photos
+      # 
+      # @return [self]
       def photo
         filter << 'Style:Photo'
         self
       end
       
       # Restrict images to those which contain graphics or illustrations
+      # 
+      # @return [self]
       def graphics
         filter << 'Style:Graphics'
         self
       end
       
       # Restrict images to those that are in black and white
+      # 
+      # @return [self]
       def monochrome
         filter << 'Color:Monochrome'
         self
       end
       
       # Restrict images to those which contain faces
+      # 
+      # @return [self]
       def face
         filter << 'Face:Face'
         self
       end
       
       # Restrict images to those which contain portraits(head and shoulders)
+      # 
+      # @return [self]
       def portrait
         filter << 'Face:Portrait'
         self
