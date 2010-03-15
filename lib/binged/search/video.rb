5ccb18f3a55ae216dbf74a8c3bd3cf05a92a0f0f
Kevin Faustino <kevin.faustino@gmail.com>
2010-03-15 09:55:43 -0400

    Add Video filters

--- lib/binged/search/video.rb
+++ lib/binged/search/video.rb
@@ -4,9 +4,10 @@ module Binged
     class Video < Base
       include Filter
       include Pageable
-      
+
+      SUPPORTED_RESOLUTIONS = [:low, :medium, :high]
       SUPPORTED_SORT_OPTIONS = [:date, :relevance]
-      
+
       # @param [Binged::Client] client
       # @param [String] query The search term to be sent to Bing
       # @param [Hash] options
@@ -15,19 +16,55 @@ module Binged
         @source = :video
         set_paging_defaults
       end
-      
+
       # Change the sorting of the video search
       #
       # @example
       #   search.sort_by(:date) # Return videos in chronological order
       #
       # @param [Symbol] type A symbol of a {SUPPORTED_SORT_OPTIONS}
-      # @return [self]  
+      # @return [self]
       # @see http://msdn.microsoft.com/en-us/library/dd560945.aspx Description of all sort options
       def sort_by(type)
         @query['Video.SortBy'] = type if SUPPORTED_SORT_OPTIONS.include?(type)
         self
       end
+
+      def short
+        filter << 'Duration:Short'
+        self
+      end
+
+      def medium
+        filter << 'Duration:Medium'
+        self
+      end
+
+      def long
+        filter << 'Duration:Long'
+        self
+      end
+
+      def standard
+        filter << 'Aspect:Standard'
+        self
+      end
+
+      def widescreen
+        filter << 'Aspect:Widescreen'
+        self
+      end
+
+      def resolution(type)
+        filter << "Resolution:#{type.to_s.capitalize}" if SUPPORTED_RESOLUTIONS.include?(type)
+        self
+      end
+
+      private
+
+        def filter
+          @query['Video.Filters'] ||= []
+        end
 
     end
 
--- spec/binged/search/video_spec.rb
+++ spec/binged/search/video_spec.rb
@@ -11,32 +11,63 @@ module Binged
         @client = Binged::Client.new(:api_key => 'binged')
         @search = Video.new(@client)
       end
-      
+
       it "should initialize with a search term" do
         Video.new(@client, 'ruby').query[:Query].should include('ruby')
       end
-      
+
       context "sorting" do
-        
+
         it "should be able to sort by date" do
           @search.sort_by(:date)
           @search.query['Video.SortBy'].should == :date
         end
-        
+
         it "should be able to sort by relevance" do
           @search.sort_by(:relevance)
           @search.query['Video.SortBy'].should == :relevance
         end
-        
+
         it "should ignore unsupported sort options" do
           @search.sort_by(:boring)
           @search.query['Video.SortBy'].should be_nil
         end
-        
+
       end
 
       context "filtering" do
-        
+
+        describe "duration" do
+
+          %w(Short Medium Long).each do |duration|
+            it "should filter by a #{duration} duration" do
+              @search.send duration.downcase.to_sym
+              @search.query['Video.Filters'].should include("Duration:#{duration}")
+            end
+          end
+
+        end
+
+        describe "aspect" do
+
+          %w(Standard Widescreen).each do |aspect|
+            it "should filter by a #{aspect} aspect ratio" do
+              @search.send aspect.downcase.to_sym
+              @search.query['Video.Filters'].should include("Aspect:#{aspect}")
+            end
+          end
+
+        end
+
+        describe "resolution" do
+          %w(Low Medium High).each do |resolution|
+            it "should filter by a #{resolution} resolution" do
+              @search.resolution resolution.downcase.to_sym
+              @search.query['Video.Filters'].should include("Resolution:#{resolution}")
+            end
+          end
+        end
+
       end
 
       context "fetching" do
