0c3b45596d007a8043fb7c4eff272b2436b8cb48
Kevin Faustino <kevin.faustino@gmail.com>
2010-03-15 09:05:28 -0400

    Add spec for image search generator in client

--- spec/binged_spec.rb
+++ spec/binged_spec.rb
@@ -11,9 +11,20 @@ describe "Binged" do
     client.api_key.should == 'api_key'
   end
 
-  it "should provide a simple interface to web search" do
-    client = Binged::Client.new
-    client.web.should be_instance_of(Binged::Search::Web)
+  describe "Flexible interface" do
+    
+    before(:each) do
+      @client = Binged::Client.new
+    end
+
+    it "should provide an interface to web search" do
+      @client.web.should be_instance_of(Binged::Search::Web)
+    end
+
+    it "should provide an interface to image search" do
+      @client.image.should be_instance_of(Binged::Search::Image)
+    end
+    
   end
 
 end
