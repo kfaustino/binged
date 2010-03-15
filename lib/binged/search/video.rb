02dab341591d32c4122151fb7245b030c69d2d72
Kevin Faustino <kevin.faustino@gmail.com>
2010-03-15 10:00:21 -0400

    Add documentation to Video filters

--- lib/binged/search/video.rb
+++ lib/binged/search/video.rb
@@ -30,31 +30,50 @@ module Binged
         self
       end
 
+      # Video duration is less than 300 seconds
+      # 
+      # @return [self]
       def short
         filter << 'Duration:Short'
         self
       end
 
+      # Video duration is between 300 seconds and 12000 seconds
+      # 
+      # @return [self]
       def medium
         filter << 'Duration:Medium'
         self
       end
 
+      # Video duration is larger than 12000 seconds
+      # 
+      # @return [self]
       def long
         filter << 'Duration:Long'
         self
       end
 
+      # Restrict videos to those which have a standard aspect ratio
+      # 
+      # @return [self]
       def standard
         filter << 'Aspect:Standard'
         self
       end
 
+      # Restrict videos to those which have a widescreen aspect ratio
+      # 
+      # @return [self]
       def widescreen
         filter << 'Aspect:Widescreen'
         self
       end
 
+      # Restrict videos to those which have a certain resolution
+      # 
+      # @param [Symbol] type A symbol of a {SUPPORTED_RESOLUTIONS}
+      # @return [self]
       def resolution(type)
         filter << "Resolution:#{type.to_s.capitalize}" if SUPPORTED_RESOLUTIONS.include?(type)
         self
