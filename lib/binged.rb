080203980d7d21978291fcbfe63a9ee4917319ea
Paul Springett <paul@simpleweb.co.uk>
2011-12-01 17:20:37 +0000

    Require activesupport's to_param method

--- lib/binged.rb
+++ lib/binged.rb
@@ -2,6 +2,7 @@ require 'crack'
 require 'hashie'
 require 'net/http'
 require 'uri'
+require 'active_support/core_ext/object'
 
 Hash.send :include, Hashie::HashExtensions
 require 'binged/hashie_extensions'
